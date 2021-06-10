937873a0b32412d450f3d7db7fb777e85b062795
VigilV11 <vigilv@pm.me>
2021-06-10 10:44:14 +0530

    add simple front-end interface

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 require('./db/mongoose');
 const userRouter = require('./routers/user');
@@ -9,6 +10,14 @@ app.use(express.json());
 
 app.use(userRouter);
 app.use(taskRouter);
+
+const publicPath = path.join(process.cwd(), 'public');
+
+app.use(express.static(publicPath));
+
+app.get('/', (req, res) => {
+  res.status(200).send('Welcome!');
+});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('Development server listening on port 3000...');
